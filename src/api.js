d7ac497c0b056524b7867f617af28b43bcbc9464
Mayur Takke <takkemayur456@gmail.com>
2026-03-17 12:55:38 +0530

    juuu

--- src/api.js
+++ src/api.js
@@ -5,6 +5,6 @@ const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
   (window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
-    : "https://eventbackend-main.onrender.com/api");
+    : "https://eventbackend-main18.onrender.com/api");
 
 export default API_BASE_URL;
